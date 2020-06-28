c405e2128d804f3b3f3e0fa782ab87420cf1cfb9
PRAJAKTA SELUKAR <34501121+PrajaktaSelukar@users.noreply.github.com>
2020-06-28 21:45:15 +0530

    Add files via upload

---
+++ Tree/Binary Tree/Basic/24. Print_nodes_having_k_leaves.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node*left;
+    struct node*right;
+};
+
+struct node*buildTree()
+{
+    struct node*root = new node;
+    cout<<"Enter Root's Data : ";
+    cin>>root->data;
+
+    queue<node*> q;
+    q.push(root);
+
+    while(!q.empty())
+    {
+
+        struct node*temp = q.front();
+        q.pop();
+        cout<<"Enter the children (or -1) for node with data as "<<temp->data<<" : ";
+        int data1;
+        int data2;
+        cin>>data1>>data2;
+
+        if(data1!=-1)
+        {
+            temp->left  = new node;
+            temp->left->data = data1;
+            q.push(temp->left);
+        }
+        else
+        {
+            temp->left = NULL;
+        }
+
+        if(data2!=-1)
+        {
+            temp->right = new node;
+            temp->right->data = data2;
+            q.push(temp->right);
+        }
+        else
+        {
+            temp->right = NULL;
+        }
+    }
+    return root;
+}
+
+void printLevelOrder(node* root)
+{
+    queue<node*> q;
+    q.push(root);
+
+    while(!q.empty())
+    {
+        node* temp = q.front();
+        cout<<temp->data<<" ";
+        q.pop();
+
+        //Enqueue left child
+        if(temp->left)
+            q.push(temp->left);
+
+        //Enqueue right child
+        if(temp->right)
+            q.push(temp->right);
+    }
+}
+
+int count_leaves(node* root, int k)
+{
+    if(root == NULL)
+        return 0;
+
+    if(root->left == NULL && root->right == NULL)
+        return 1;
+
+    int left_count = count_leaves(root->left, k);
+    int right_count = count_leaves(root->right, k);
+
+    int total_leaf_count = left_count + right_count;
+
+    if(total_leaf_count == k)
+        cout<<root->data<<" ";
+
+    return total_leaf_count;
+}
+
+int main()
+{
+    node* root;
+    root = buildTree();
+
+    cout<<"\nPrint nodes having k leaves\n ";
+    cout<<"Enter k : ";
+    int k;
+    cin>>k;
+
+    count_leaves(root, k);
+}
