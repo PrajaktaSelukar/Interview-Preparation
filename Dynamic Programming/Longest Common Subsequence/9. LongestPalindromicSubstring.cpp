361db6eea1a4e7f6f608206542a7cc3c780b05ec
PRAJAKTA SELUKAR <34501121+PrajaktaSelukar@users.noreply.github.com>
2020-07-27 20:20:02 +0530

    Add files via upload

---
+++ Dynamic Programming/Longest Common Subsequence/9. LongestPalindromicSubstring.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string.h>
+using namespace std;
+
+int LongestCommonSequence(string X, string Y, int n, int m)
+{
+	int dp[n+1][m+1];
+	
+	for(int i=0; i<=n; i++)
+		dp[i][0]=0;
+	
+	for(int j=0; j<=m; j++)
+		dp[0][j]=0;	
+	
+	for(int i=1; i<=n; i++)
+	{
+		for(int j=1; j<=m; j++)
+		{
+			if(X[i-1] == Y[j-1])
+				dp[i][j] = 1 + dp[i-1][j-1];
+			
+			else
+			{
+				dp[i][j] = 0;		
+			}
+				
+		}
+	}
+	
+	return dp[n][m];
+}
+
+int main()
+{
+	string X, Y;
+	cin>>X;
+	
+	Y=X;
+	reverse(Y.begin(), Y.end());
+	cout<<LongestCommonSequence(X, Y, X.length(), Y.length());
+}
