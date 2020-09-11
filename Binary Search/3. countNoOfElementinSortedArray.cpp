253ca8aadbb1210b95d2dcd03fb1454de85edf81
PRAJAKTA SELUKAR <34501121+PrajaktaSelukar@users.noreply.github.com>
2020-09-11 23:54:32 +0530

    Add files via upload

---
+++ Binary Search/3. countNoOfElementinSortedArray.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+
+int firstOccurrence(int arr[], int n, int ele)
+{
+    int start=0;
+    int end=n-1;
+    int first=-1;
+
+    while(start <= end)
+    {
+        int mid = start + (end-start)/2;
+
+        if(ele == arr[mid])
+        {
+            first=mid;
+            end=mid-1;
+        }
+
+        else if(ele < arr[mid])
+            end = mid-1;
+
+        else
+            start = mid+1;
+    }
+    return first;
+}
+
+int lastOccurrence(int arr[], int n, int ele)
+{
+    int start=0;
+    int end=n-1;
+    int last=-1;
+
+    while(start <= end)
+    {
+        int mid = start + (end-start)/2;
+
+        if(ele == arr[mid])
+        {
+            last=mid;
+            start=mid+1;
+        }
+
+        else if(ele < arr[mid])
+            end = mid-1;
+
+        else
+            start = mid+1;
+    }
+    return last;
+}
+
+int main()
+{
+    int n, ele;
+    cin>>n>>ele;
+
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
+
+    int first = firstOccurrence(arr, n, ele);
+    int last = lastOccurrence(arr, n, ele);
+
+    cout<<"Count of element "<<ele<<" : "<<last-first+1<<endl;
+}
+
